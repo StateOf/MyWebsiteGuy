02a0a396e400a58505ab77f90b7ebaddabd73b8d
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-04 13:14:08 -0700

    still trying to make it through FAQ story

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'terms', to: 'terms#index'
   get 'about', to: 'about#index'
-  get 'faq', to: 'faq#index'
+  get 'faq', to: 'common_questions#index'
 
 end
