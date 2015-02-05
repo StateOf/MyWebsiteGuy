c8b2c919b3e5ccd7ca7446dd17c8bb1043e92782
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-05 15:18:31 -0700

    added tasks to routes.rb and tasks_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'terms', to: 'terms#index'
   get 'about', to: 'about#index'
   get 'faq', to: 'common_questions#index'
+  get 'tasks', to: 'tasks#index'
 
 end
