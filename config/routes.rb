0f7ef94c6f29cff2233dd1a7139e49196bc84685
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-10 15:03:41 -0700

    fixed the links on index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'terms', to: 'terms#index'
   get 'about', to: 'about#index'
   get 'faq', to: 'common_questions#index'
-  get 'tasks', to: 'tasks#index'
+  #get 'tasks', to: 'tasks#index'
   resources :tasks
 
 
