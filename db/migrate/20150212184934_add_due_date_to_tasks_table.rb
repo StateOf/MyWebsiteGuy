34d13a1ec0b11235422bba2d1142989f2acfda20
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-12 13:54:06 -0700

    added date and formated it correctly

---
+++ db/migrate/20150212184934_add_due_date_to_tasks_table.rb
@@ -0,0 +1,5 @@
+class AddDueDateToTasksTable < ActiveRecord::Migration
+  def change
+    add_column :tasks, :due_date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211212341) do
+ActiveRecord::Schema.define(version: 20150212184934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150211212341) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "complete"
+    t.date     "due_date"
   end
 
 end
