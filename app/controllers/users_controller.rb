d8cdd4fc9b1c543bbf1d5b78da6a0aed1d2163eb
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-13 14:42:18 -0700

    fixed spacing, add users and new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User was successfully created"
+      redirect_to users_path
+    end
+  end
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
   end
