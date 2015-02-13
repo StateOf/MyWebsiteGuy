ee32542d6e3f04df5521b85f7a1b34c581ecae00
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-13 11:50:46 -0700

    add users table and model

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'terms', to: 'terms#index'
   get 'about', to: 'about#index'
   get 'faq', to: 'common_questions#index'
-  #get 'tasks', to: 'tasks#index'
   resources :tasks
+  resources :users
 
 
 end
