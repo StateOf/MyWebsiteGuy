e04afb5251ea6b2861f89dd9b4400a7dcbbbd4e7
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-25 15:03:40 -0700

    added feature task spec

---
+++ spec/features/task_management_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe "the user using the tasks" do
+  it "uses tasks complete" do
+    visit tasks_path
+    expect(page).to have_content "Tasks"
+
+    click_on "New Task"
+
+    fill_in "Description", with: "hike"
+    fill_in "Due date", with: "02/03/2015"
+
+    click_on "Create Task"
+
+    expect(page).to have_content "hike"
+    expect(page).to have_content "03/02/2015"
+
+    click_on "Edit"
+
+    fill_in "Description", with: "run"
+    fill_in "Due date", with: "02/02/1983"
+    check "Complete"
+
+    click_on "Update Task"
+
+    expect(page).to have_content "run"
+    expect(page).to have_content "02/02/1983"
+
+    visit tasks_path
+
+    click_on "Delete"
+    save_and_open_page
+    expect(page).to_not have_content "run"
+    expect(page).to_not have_content "02/02/1983"
+
+  end
+end
