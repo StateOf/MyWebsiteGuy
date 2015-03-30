85332a7c9b86778645f2a85eba4262938e6c4a1f
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-03-30 09:55:59 -0600

    permissions tested in rspec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -10,6 +10,7 @@ describe ApplicationController do
       user = create_user
       session[:user_id] = user.id
       expect(subject.current_user).to eq (User.find_by(id: session[:user_id]))
+      
     end
   end
 end
